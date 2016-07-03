c2e10d4461def8a71e71a19eaf56f757fa2765f1
mayank26saxena <mayank26saxena@gmail.com>
2016-07-03 15:31:56 +0530

    added library code to app

--- app/src/main/java/com/example/mayank/demolibrary/MainActivity.java
+++ app/src/main/java/com/example/mayank/demolibrary/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.mayank.demolibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mayank.awesomelibrary.AwesomeLibrary;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AwesomeLibrary.getInstance().makeMeAwesome(getApplicationContext(), "Mayank");
+
+
     }
 }
